8a1fe9578e216a96b3fa3706c522014c44342801
cedricgautier <75094434+cedricgautier@users.noreply.github.com>
2022-04-01 03:24:23 +0200

    Correctives for MainActivity (IDE Suggestions), added redirections to login and register pages

--- app/src/main/java/com/example/boken/MainActivity.java
+++ app/src/main/java/com/example/boken/MainActivity.java
@@ -1,15 +1,46 @@
 package com.example.boken;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.ImageView;
-
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn_login = findViewById(R.id.button_login);
+        Button btn_register = findViewById(R.id.button_register);
+
+        btn_login.setOnClickListener(view -> {
+            Intent login_page = new Intent(MainActivity.this, Login.class);
+            startActivity(login_page);
+        });
+
+        btn_register.setOnClickListener(view -> {
+            Intent register_page = new Intent(MainActivity.this, Register.class);
+            startActivity(register_page);
+        });
     }
+
+    @Override
+    public void onBackPressed () {
+        quit();
+    }
+
+    public void quit() {
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Do you want to Quit")
+                .setPositiveButton("Yes", (dialog, which) -> finish())
+                .setNegativeButton("No", (dialog, which) -> {
+                })
+                .setCancelable(false)
+                .show();
+    }
+
 }
