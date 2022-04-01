8745f13ea11126c43f4673cc9f3ea231acfc8168
cedricgautier <75094434+cedricgautier@users.noreply.github.com>
2022-04-01 06:35:02 +0200

    Added hint to login page

--- app/src/main/java/com/example/boken/Login.java
+++ app/src/main/java/com/example/boken/Login.java
@@ -1,14 +1,19 @@
 package com.example.boken;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EditText login_email =  findViewById(R.id.login_email);
+        EditText login_password = findViewById(R.id.login_password);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
+
 }
